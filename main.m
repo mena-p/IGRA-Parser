9fcf96d3a0bb519f4b81ddfe95c69676f45394a2
mena-p <119264054+mena-p@users.noreply.github.com>
2024-07-16 18:37:02 +0200

    Updated usage

--- main.m
+++ main.m
@@ -2,6 +2,13 @@
 clear
 
 % Usage:
-exampleOutput = parse_derived('GMM00010868-drvd.txt');
 
+% Parse derived parameter file
+parsed_soundings = parse_derived('test_data.txt');
+
+% Filter soundings based on the presence of the inversion height parameter
+filtered_soundings = filter_soundings(parsed_soundings);
+
+% Extract and return only the relevant data in a sounding
+reduced_sounding = extract_sounding_data(filtered_soundings(1));
 
