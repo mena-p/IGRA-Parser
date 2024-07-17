d2f6c18da043c88b16c53c60a5b36b30dda7cb86
mena-p <119264054+mena-p@users.noreply.github.com>
2024-07-17 18:34:48 +0200

    Updated usage to reflect new functions

--- main.m
+++ main.m
@@ -2,27 +2,31 @@
 clear
 close
 
-% Usage:
+% Workflow:
 
 % Parse derived parameter file
 parsed_soundings = parse_derived('test_data.txt');
 
-% Filter soundings based on the presence of the mixed layer height parameter
+% Filter soundings based on the presence of the mixed layer height
+% parameter. Soundings where the parameter does not exist are removed.
 filtered_soundings = filter_soundings(parsed_soundings);
 
-% Save the filtered_soundings variable to a MAT file compatible with Simulink
-save('filtered_soundings_simulink.mat', 'filtered_soundings', '-v7.3');
-
-clear
-
-% Load the saved variable from the MAT file in Simulink
-load('filtered_soundings_simulink.mat');
-
-% Extract and return only the relevant data in a sounding
+% Extract and return only the relevant data in a sounding. Extracts
+% geopotential height, pressure, temperature, potential temperature and
+% virtual temperature. Missing reported geopotential heights (REPGPH) 
+% are filled with calculated values (CALCGPH).
 reduced_sounding = extract_sounding_data(filtered_soundings(1));
 
+% Remove all measurements that have a geopotential height REPGPH
+% greater than 1.3 times the mixed layer height.
+capped_sounding = remove_values_above(reduced_sounding, 1.3);
+
+% Interpolate missing values in the sounding with an interval of 1 meter
+reduced_sounding = interpolate_missing(reduced_sounding);
+
 % Plot pressure, temperature, potential temperature and virtual 
-% temperature profiles over geopotential height. The geoportential 
-% height is used as the vertical coordinate.
-plot_sounding(reduced_sounding, 1.3);
+% temperature profiles over geopotential height, up to 1.1 times the
+% mixed layer height.
+plot_sounding(reduced_sounding, 1.1);
+
 
