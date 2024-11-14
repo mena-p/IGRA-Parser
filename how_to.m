929848a007d1c173658b8a679cc02bcc63798c37
mena-p <119264054+mena-p@users.noreply.github.com>
2024-11-14 22:03:18 +0100

    Update how_to.m

--- how_to.m
+++ how_to.m
@@ -1,11 +1,11 @@
 %% Find active stations
-
+clear
 % Specify the location and date where you want to search for active stations
-location = [40.7128 -74.0060]; % New York City
+location = [40.7128 -74.0060]; % [lat lon] New York
 date = datetime('19 July 2023','Format','dd MMMM yyyy','TimeZone','UTC');
 
 % Find active stations
-active_stations = find_active_stations(location, date,10000);
+active_stations = find_active_stations(location,date,10000); % also specify the starting search radius in meters
 
 % This will search for and download the data for the active stations from the IGRA database.
 
@@ -18,3 +18,5 @@ found_soundings = [];
     end
 
 % This will search for soundings on the specified date at the active stations.
+% It will return an array of sounding objects.The sounding data is stored 
+% in the "derived" attribute of the sounding object.
